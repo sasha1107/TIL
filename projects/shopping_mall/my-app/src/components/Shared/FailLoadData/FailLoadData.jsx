889e831a8c5bb22c2464447e7ba9bcff3480fd6a
Soohyun Jung <sasha1107@naver.com>
2022-11-28 01:37:58 +0900

    Main 브랜치 초기화

--- projects/shopping_mall/my-app/src/components/Shared/FailLoadData/FailLoadData.jsx
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-
-export default function FailLoadData() {
-    return (
-        <div>서버 오류</div>
-    )
-}
--- projects/shopping_mall/my-app/src/database/callAPI.js
+++
@@ -1,9 +0,0 @@
-import { createContext } from 'react'
-
-async function callAPI(){
-    const res = await fetch('http://test.api.weniv.co.kr/mall')
-    const result = await res.json()
-    return result
-}
-
-export const callAPIResponse = createContext(callAPI())
